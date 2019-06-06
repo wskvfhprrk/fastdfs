06656498a809a81ffac3591e0b20429337a2b7b1
he <75412985@qq.com>
2019-06-06 12:40:11 +0800

    初始化代码

--- src/main/java/com/example/fastdfs/FastDFSClientWrapper.java
+++ src/main/java/com/example/fastdfs/FastDFSClientWrapper.java
@@ -7,11 +7,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Base64Utils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
+import sun.misc.BASE64Encoder;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 import java.nio.charset.Charset;
 
 @Slf4j
@@ -26,25 +31,27 @@ public class FastDFSClientWrapper {
 
     /**
      * 上传文件
+     *
      * @param file 文件对象
      * @return 文件访问地址
      * @throws IOException
      */
     public String uploadFile(MultipartFile file) throws IOException {
-        StorePath storePath = storageClient.uploadFile(file.getInputStream(),file.getSize(), FilenameUtils.getExtension(file.getOriginalFilename()),null);
+        StorePath storePath = storageClient.uploadFile(file.getInputStream(), file.getSize(), FilenameUtils.getExtension(file.getOriginalFilename()), null);
         return getResAccessUrl(storePath);
     }
 
     /**
      * 将一段字符串生成一个文件上传
-     * @param content 文件内容
+     *
+     * @param content       文件内容
      * @param fileExtension
      * @return
      */
     public String uploadFile(String content, String fileExtension) {
         byte[] buff = content.getBytes(Charset.forName("UTF-8"));
         ByteArrayInputStream stream = new ByteArrayInputStream(buff);
-        StorePath storePath = storageClient.uploadFile(stream,buff.length, fileExtension,null);
+        StorePath storePath = storageClient.uploadFile(stream, buff.length, fileExtension, null);
         return getResAccessUrl(storePath);
     }
 
@@ -57,6 +64,7 @@ public class FastDFSClientWrapper {
 
     /**
      * 删除文件
+     *
      * @param fileUrl 文件访问地址
      * @return
      */
@@ -71,4 +79,47 @@ public class FastDFSClientWrapper {
             log.warn(e.getMessage());
         }
     }
+
+    /**
+     * 在线图片转换成base64字符串
+     *
+     * @param imgURL 图片线上路径
+     * @return
+     * @author ZHANGJL
+     * @dateTime 2018-02-23 14:43:18
+     */
+    public static String ImageToBase64ByOnline(String imgURL) {
+        ByteArrayOutputStream data = new ByteArrayOutputStream();
+        try {
+            // 创建URL
+            URL url = new URL(imgURL);
+            byte[] by = new byte[1024];
+            // 创建链接
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setConnectTimeout(5000);
+            InputStream is = conn.getInputStream();
+            // 将内容读取内存中
+            int len = -1;
+            while ((len = is.read(by)) != -1) {
+                data.write(by, 0, len);
+            }
+            // 关闭流
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // 对字节数组Base64编码
+        BASE64Encoder encoder = new BASE64Encoder();
+        String px = imgURL.substring(imgURL.lastIndexOf(".")+1, imgURL.length());
+        String s = "data:image/" + px + ";base64," + encoder.encode(data.toByteArray());
+        return s;
+    }
+
+    public static void main(String[] args) {
+        String s = "http://192.168.166.183:8080/group1/M00/00/01/wKimt1zw-X2ASH3gAADVd2_l6tM08.jpeg";
+        String s1 = ImageToBase64ByOnline(s);
+        System.out.println(s1);
+    }
+
 }
--- src/main/java/com/example/fastdfs/FastDFSController.java
+++ src/main/java/com/example/fastdfs/FastDFSController.java
@@ -1,9 +1,8 @@
 package com.example.fastdfs;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -16,4 +15,10 @@ public class FastDFSController {
         String imgUrl = dfsClient.uploadFile(file);
         return imgUrl;
     }
+    @GetMapping(value = "auth")
+    public void auth(){
+        throw new ForbiddenException();
+    }
+    @ResponseStatus(value = HttpStatus.FORBIDDEN, reason="To show an example of a custom message")
+    public class ForbiddenException extends RuntimeException {}
 }
